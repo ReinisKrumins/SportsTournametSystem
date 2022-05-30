14fd2ba67f3dba98de013c33360939dac596a6c1
pknowledge <sdaqit96@gmail.com>
2022-05-30 11:13:15 +0300

    Project Updates from Sdaqit

--- src/main/java/com/StSystem/SportsTournamentSystemApplication.java
+++ src/main/java/com/StSystem/SportsTournamentSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootAndSqLiteApplication {
+public class SportsTournamentSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAndSqLiteApplication.class, args);
+		SpringApplication.run(SportsTournamentSystemApplication.class, args);
 	}
 
 }
